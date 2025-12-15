c0dd3693f9da480bf893d6d88ffed2cdc26bfce0
Adlina <adlinaabdulsamad@gmail.com>
2025-12-15 13:16:14 +0800

    Commit Another Another version

--- testing1.py
+++ testing1.py
@@ -1,5 +1,17 @@
 def sub(a,d):
     return d - a
 
+class Car: 
+    def __init__(self,brand,name,year):
+        self.brand = brand
+        self.name = name
+        self.year = year 
+        
+    def __str__(self):
+        return(f"Hoho Yes {self.brand},{self.name},{self.year}" )
+        
+Car1 = Car("Honda","City",1997)
+print(Car1)
+
 testing3 = sub(10,80)
 print(testing3)
